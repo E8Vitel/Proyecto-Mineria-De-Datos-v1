a7355d8befa9e03e2d5c88ad50ecdb5be4e3224d
E8Vitel <vitklalvz@gmail.com>
2024-12-07 18:00:29 -0300

    Actualizacion index.php y estilos css

--- index.php
+++ index.php
@@ -3,9 +3,92 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- AOS CSS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- CSS -->
+     <link rel="stylesheet" href="css/styles.css">
+    <title>Inicio</title>
 </head>
 <body>
+    <div class="border border-primary mt-3 mb-3">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary nav-sup">
+            <div class="container-fluid">
+                <img src="img/Icon-data.png" alt="Icono de pagina">  
+                <a class="navbar-brand" href="#">Mineria de datos</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDespliegue" aria-controls="navbarDespliegue" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarDespliegue">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active fs-5 " aria-current="page" href="#"><span class="bordes">Inicio</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fs-5" aria-current="page" href="#"><span class="bordes">Docs</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fs-5" aria-current="page" href="#"><span class="bordes">Ejercicios</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
     
+    <div class="container-fluid contenedor">
+        <h2>Inicio</h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis ante vitae risus vulputate pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam pulvinar, odio ut malesuada volutpat, sem augue facilisis magna, tristique gravida nibh tortor id est. Curabitur sagittis diam eu volutpat sagittis. Suspendisse potenti. Cras a ex dictum, sollicitudin neque ut, pulvinar erat. Pellentesque eu volutpat mauris. Morbi in molestie ligula. Quisque et nisi sagittis, tincidunt quam id, consectetur enim. Nam turpis mauris, ullamcorper sed auctor ac, lobortis in dui. In a molestie risus. Mauris commodo ut nulla sit amet commodo. Vestibulum aliquam posuere nulla, egestas interdum massa ultricies a. Nunc sed porttitor justo. Nam sagittis ac urna et tempus. Nullam mattis pellentesque diam id facilisis.
+        </p>
+
+        <p>Maecenas in risus bibendum, luctus risus a, rutrum lacus. Cras tempus orci ac est aliquet, id pulvinar lorem faucibus. Praesent eget semper dolor. Morbi in rhoncus velit, id porta est. Morbi convallis libero eros, convallis finibus lectus dignissim quis. Proin commodo hendrerit interdum. Donec pharetra felis sed purus fermentum mattis. Nunc augue mauris, fringilla sit amet porttitor ut, mattis eget massa. Vestibulum commodo viverra quam in aliquet. Sed orci lorem, lobortis eu condimentum ut, placerat vel mi. Nam non accumsan diam, sed ultricies orci. Etiam blandit vitae eros sit amet porta. Integer interdum nec mi vitae imperdiet. Curabitur et imperdiet lorem. Pellentesque dignissim mi justo, in pellentesque ligula blandit ut. </p>
+
+        <div class="custom-grid">
+            
+            <div class="texto">
+                <h1>Informacion Mineria de datos</h1>
+                <p>
+                    Texto de mineria de datos
+                </p>
+            </div>
+
+            <div class="imagenes">
+                <img src="https://via.placeholder.com/200" alt="Imagen 1">
+                <img src="https://via.placeholder.com/200" alt="Imagen 2">
+                <img src="https://via.placeholder.com/200" alt="Imagen 3">
+                <img src="https://via.placeholder.com/200" alt="Imagen 4">
+            </div>
+        </div>
+
+        <div class="custom-grid">
+            <div class="imagenes">
+                <img src="https://via.placeholder.com/200" alt="Imagen 1">
+                <img src="https://via.placeholder.com/200" alt="Imagen 2">
+                <img src="https://via.placeholder.com/200" alt="Imagen 3">
+                <img src="https://via.placeholder.com/200" alt="Imagen 4">
+            </div>
+
+            <div class="texto">
+                <h1>Texto D3 js</h1>
+                <p>
+                    Aca se explica la libreria de JavaScript utilizada para la muestra de graficos.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    </div>
+
+
+    <!-- Script Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Script AOS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
