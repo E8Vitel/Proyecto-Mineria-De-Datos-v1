b9ec359479d7716521610c94800acfca570d5320
E8Vitel <vitklalvz@gmail.com>
2024-12-12 12:56:41 -0300

    cambios minimos

--- nav.php
+++ nav.php
@@ -2,14 +2,14 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary nav-sup">
         <div class="container-fluid">
             <img src="img/Icon-data.png" class="icono" alt="Icono de pagina">  
-            <a class="navbar-brand" href="index.html">Mineria de datos</a>
+            <a class="navbar-brand" href="index.php">Mineria de datos</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDespliegue" aria-controls="navbarDespliegue" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarDespliegue">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active fs-5 " aria-current="page" href="index.html"><span class="bordes">Inicio</span></a>
+                        <a class="nav-link active fs-5 " aria-current="page" href="index.php"><span class="bordes">Inicio</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active fs-5" aria-current="page" href="documentacion.php"><span class="bordes">Docs</span></a>
